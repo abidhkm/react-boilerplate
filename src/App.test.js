87d6d7082ac94259eee0d9a5c51c959f2ff07045
muhammed abidh <abidh.k@appsintegra.com>
2019-10-09 23:11:33 +0400

    jest config added

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { shallow } from 'enzyme';
+import App from './App'
+
+it('should render correctly', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).toMatchSnapshot();
+})
